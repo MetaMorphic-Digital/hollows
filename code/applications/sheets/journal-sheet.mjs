87f2c0f8bec4f5a88362f292c8fcf56004e84cb7
jalensailin <jalen.michalslevy@gmail.com>
2026-08-25 01:07:46 -0400

    Donut-scope color variables

--- code/applications/sheets/journal-sheet.mjs
+++ code/applications/sheets/journal-sheet.mjs
@@ -2,5 +2,5 @@ const { JournalEntrySheet } = foundry.applications.sheets.journal;
 
 export default class HollowsJournalEntrySheet extends JournalEntrySheet {
   /** @inheritdoc */
-  static DEFAULT_OPTIONS = { classes: ["hollows"] };
+  static DEFAULT_OPTIONS = { classes: ["hollows", "themed", "theme-light"] }; // Force light theme with the last two vars.
 }
