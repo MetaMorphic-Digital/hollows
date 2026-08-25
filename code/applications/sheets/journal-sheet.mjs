699e4f8f8b5e1d8ae9f9e03f6687c63f2573dbb7
Zhell <kennethzhell@gmail.com>
2026-08-25 14:55:23 +0200

    Adjust class application on journal entry sheet. Set contextmenus to be `fixed`.

--- code/applications/sheets/journal-sheet.mjs
+++ code/applications/sheets/journal-sheet.mjs
@@ -1,6 +1,33 @@
 const { JournalEntrySheet } = foundry.applications.sheets.journal;
 
+/**
+ * System journal entry sheet.
+ * @extends foundry.applications.sheets.journal.JournalEntrySheet
+ */
 export default class HollowsJournalEntrySheet extends JournalEntrySheet {
   /** @inheritdoc */
-  static DEFAULT_OPTIONS = { classes: ["hollows", "themed", "theme-light"] }; // Force light theme with the last two vars.
+  static DEFAULT_OPTIONS = {};
+
+  /* -------------------------------------------------- */
+
+  /** @inheritdoc */
+  _initializeApplicationOptions(options) {
+    const applicationOptions = super._initializeApplicationOptions(options);
+    const classes = new Set(applicationOptions.classes);
+    classes.add(hollows.id);
+
+    // Force light theme
+    classes.add("themed").add("theme-light").delete("theme-dark");
+    applicationOptions.classes = Array.from(classes);
+
+    return applicationOptions;
+  }
+
+  /* -------------------------------------------------- */
+
+  /** @inheritdoc */
+  _createContextMenu(handler, selector, options = {}) {
+    options.fixed ??= true;
+    return super._createContextMenu(handler, selector, options);
+  }
 }
